5f1868eb837328481818337741cd440711c03f11
Mohamed Bakr <mbma_2002@hotmail.com>
2017-05-21 21:55:24 +0300

    Add Sample App

---
+++ TestDapperIdentity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDapperIdentity.MvcApplication" Language="C#" %>
---
+++ TestDapperIdentity/Models/IdentityModels.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using AspNet.Identity.Dapper;
+using Dapper;
+using System.Data.Common;
+
+namespace TestDapperIdentity.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    [Table("AspNetusers")]
+    public class ApplicationUser : IdentityUser , IDapperIdentity<ApplicationUser>
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext 
+    {
+        public static DbConnection Create()
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            return new System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+}
